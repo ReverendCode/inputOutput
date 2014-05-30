93ae6e2365248316f174818826c8a158aa8d6916
David Wenzel <Code@False.local>
2014-05-29 18:20:26 -0700

    Initial Commit

---
+++ inputOutput/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  inputOutput
+//
+//  Created by David Wenzel on 5/29/14.
+//  Copyright (c) 2014 David Wenzel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
